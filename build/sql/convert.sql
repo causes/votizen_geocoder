36fa9e94c551028845e7f54ede64fcee0022705a
Schuyler Erle <schuyler@nocat.net>
2012-01-08 23:12:18 +0000

    Revert "Update to use TIGER/Line 2011 address range feature files."
    This reverts commit e1e43e1c1ed66f801b0135223646265c88527c5c.

--- build/sql/convert.sql
+++ build/sql/convert.sql
@@ -1,17 +1,20 @@
 BEGIN;
 -- start by indexing the temporary tables created from the input data.
 CREATE INDEX featnames_tlid ON tiger_featnames (tlid);
-CREATE INDEX addrfeat_tlid ON tiger_addrfeat (tlid);
+CREATE INDEX addr_tlid ON tiger_addr (tlid);
+CREATE INDEX edges_tlid ON tiger_edges (tlid);
 
 -- generate a summary table matching each edge to one or more ZIPs
 --   for those edges that are streets and have a name
 CREATE TEMPORARY TABLE linezip AS
     SELECT DISTINCT tlid, zip FROM (
-        SELECT tlid, zipr AS zip FROM tiger_addrfeat e
-           WHERE e.edge_mtfcc LIKE 'S%' AND zipr <> "" AND zipr IS NOT NULL
+        SELECT tlid, zip FROM tiger_addr a
         UNION
-        SELECT tlid, zipl AS zip FROM tiger_addrfeat e
-           WHERE e.edge_mtfcc LIKE 'S%' AND zipl <> "" AND zipl IS NOT NULL
+        SELECT tlid, zipr AS zip FROM tiger_edges e
+           WHERE e.mtfcc LIKE 'S%' AND zipr <> "" AND zipr IS NOT NULL
+        UNION
+        SELECT tlid, zipl AS zip FROM tiger_edges e
+           WHERE e.mtfcc LIKE 'S%' AND zipl <> "" AND zipl IS NOT NULL
     ) AS whatever;
 
 CREATE INDEX linezip_tlid ON linezip (tlid);
@@ -64,17 +67,14 @@ INSERT OR IGNORE INTO edge
 -- generate all ranges from the addr table, stripping off any non-digit
 --   prefixes and putting them in a separate column.
 INSERT INTO range
-    SELECT tlid, digit_suffix(lfromhn), digit_suffix(ltohn),
-           nondigit_prefix(lfromhn), zipl, 'L'
-    FROM tiger_addr
-    UNION
-    SELECT tlid, digit_suffix(rfromhn), digit_suffix(rtohn),
-           nondigit_prefix(rfromhn), zipr, 'R'
+    SELECT tlid, digit_suffix(fromhn), digit_suffix(tohn),
+           nondigit_prefix(fromhn), zip, side
     FROM tiger_addr;
 END;
 
 DROP TABLE feature_bin;
 DROP TABLE linezip;
-DROP TABLE tiger_addrfeat;
+DROP TABLE tiger_addr;
 DROP TABLE tiger_featnames;
+DROP TABLE tiger_edges;
 
